ab4fe9d07fdc0a1b02c28bd866e097bd10cfbd26
Maria Semenets <semenets@wisc.edu>
2020-02-08 16:53:41 -0600

    Initial commit

---
+++ app/src/main/java/com/theoneandonly/currencyconverter/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.theoneandonly.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        Double dollars = Double.parseDouble(intent.getStringExtra("message"));
+        textView2.setText("$" + dollars + " is £" + dollars*.77);
+    }
+}
